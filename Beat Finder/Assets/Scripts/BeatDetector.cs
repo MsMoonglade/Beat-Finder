8634a023ea72e6508199d03037880fbdbd939721
MsMoonglade <moonglade95@gmail.com>
2023-12-29 17:58:25 +0100

    Mid Beat Frequency

--- Beat Finder/Assets/Scripts/BeatDetector.cs
+++ Beat Finder/Assets/Scripts/BeatDetector.cs
@@ -5,10 +5,13 @@ using UnityEngine.UIElements;
 
 public class BeatDetector : MonoBehaviour
 {
+    public int partPerBpm;
+
     public AudioSource songSource;
     public AudioSource clipSource;
 
     private Coroutine beatCoroutine;
+
 
     void Start()
     {
@@ -35,15 +38,39 @@ public class BeatDetector : MonoBehaviour
 
         while (true)
         {
+            float[] spectrum = new float[128];
+
+            AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+
             time += Time.deltaTime;
 
-            if (time >= beatInterval)
+            if (time >= (beatInterval / partPerBpm ))
             {
-                clipSource.Play();
-                time -= beatInterval;
+                //clipSource.Play();
+
+                float value = ReturnAverage(spectrum);
+
+                if(value>= 0.5f)
+                    clipSource.Play();
+
+                Debug.Log(value);
+
+                time =0 ;
             }
 
             yield return null;
         }
+    }
+
+    private float ReturnAverage(float[] i_initial)
+    {
+        float o_average = 0;
+
+        foreach (float i in i_initial)
+        {
+            o_average += i;
+        }
+         
+        return (o_average / i_initial.Length) * 100;
     }
 }
